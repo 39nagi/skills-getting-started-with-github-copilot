df6bfefc521161607f7a4aedf242d5ef79c3b56b
nagi <nagi.39@outlook.jp>
2025-03-23 13:24:53 +0000

    Change design to show current participants in tag style

--- src/static/app.js
+++ src/static/app.js
@@ -25,7 +25,10 @@ document.addEventListener("DOMContentLoaded", () => {
           <p>${details.description}</p>
           <p><strong>Schedule:</strong> ${details.schedule}</p>
           <p><strong>Availability:</strong> ${spotsLeft} spots left</p>
-          <p><strong>Current Participants:</strong> ${details.participants.length > 0 ? details.participants.join(", ") : "None"}</p>
+          <p class="participants"><strong>Current Participants:</strong></p>
+          <p>${
+            details.participants.map(participant => `<span class="tag">${participant}</span>`).join("")
+          }</p>
         `;
 
         activitiesList.appendChild(activityCard);
